fd6b2fea4c518307603e4eccb2c5212592d2e723
mzruiz <matthew.z.ruiz@gmail.com>
2022-03-07 19:05:21 -0500

    Updates

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,7 +4,7 @@ import getAccessToken from "./twilio/getAccessToken";
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const getTwilioAccessToken = functions.https.onRequest((request: any) => {
+export const getTwilioAccessToken = functions.https.onCall((request: any) => {
   functions.logger.log("getTwilioAccessToken");
   getAccessToken();
 });
